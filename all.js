0c9e9b994db46e2b5389832967664c1be05770ae
amirulislam26 <26techamirulislam@gmail.com>
2022-09-29 21:33:24 -0700

    my love js and my all provlem solved

---
+++ all.js
@@ -0,0 +1,28 @@
+/*
+*01:................................
+* Do you know how JavaScript works?
+ * 
+ *  JavaScript is a client side scripting or browser scripting language. JavaScript (abbreviated as JS) is a prototype-based scripting language with variables, weak types, and first-class functions. It is an object oriented, functional programming style supporting language.
+*02:...............................
+*"What are variables?"
+ * 
+ *  A variable is something whose value can change over time. whose value can be affected in different ways over time and can be used to express mathematical values ​​or something.
+ * 
+ *  03:..............................
+ * What are the primitive and non-primitive data types in JavaScript? Give an example.
+ *
+ * Strings, Numbers, Boolean, Null, undefined
+ * 
+ * 04..............................
+ * How to declare variable names. No thing can be written in the name. That is, tell about the naming convention of Variable.
+ * 
+ * To store any data in programming... that is, the variable is the name of the memory address,...
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
