13ad976f06f1a1bcdc7399ef7773ad477a1299e9
amirulislam26 <26techamirulislam@gmail.com>
2022-10-09 08:05:29 -0700

    added my js conversion

---
+++ conversion.js
@@ -0,0 +1,40 @@
+// my name is amirul islam
+var firstName = 'Amirul';
+var listName = 'islam';
+var fullName = firstName + listName;
+
+// console.log(fullName);
+//=====================
+var firstName = 'Amirul';
+var listName = 'islam';
+var fullName = firstName +' '+ listName;
+
+// console.log(fullName);
+//..............oky.......................
+var love = '80';
+var yourLove = 20;
+
+var totalLove = love + yourLove;
+// console.log(totalLove);
+
+var love = 80;
+var yourLove = '20';
+
+var totalLove = love + yourLove;
+// console.log(totalLove);
+
+var love = '80';
+var yourLove = 20;
+
+var totalLove = love + yourLove;
+// console.log(totalLove);
+var iLove = parseInt(love);
+// console.log(iLove);
+
+var love = '80.45';
+var yourLove = 20;
+
+var totalLove = love + yourLove;
+// console.log(totalLove);
+var iLove = parseFloat(love);
+console.log(iLove);
