fb9c60b4b7ea8ce393a2806ce9ad78d09c4519d8
amirulislam26 <26techamirulislam@gmail.com>
2022-10-09 09:55:09 -0700

    array-nunber-setup added

---
+++ array-nunber-setup.js
@@ -0,0 +1,10 @@
+var num = [45, 54, 78, 45, 62, 15, 23, 211];
+//get element by index
+var element = num[5];
+console.log(element);
+//set element value by index
+num[5] = 77;
+num[4] = 254;
+num[3] = 577;
+console.log(num);
+
