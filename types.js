d0ffc28047126c6f87e8fc8c4edb859d454770b0
amirulislam26 <26techamirulislam@gmail.com>
2022-10-09 08:24:07 -0700

    types js added my commit

---
+++ types.js
@@ -0,0 +1,31 @@
+var love = 80;
+console.log(typeof love);
+
+var love = '80';
+console.log(typeof love);
+
+var love = true;
+console.log(typeof love);
+
+var love = false;
+console.log(typeof love);
+
+var romantic;
+console.log(typeof romantic);
+//.............................
+var love = 80;
+var yourLove = 20;
+var sum = love + yourLove;
+console.log(sum);
+//.............................
+var love = 0.1;
+var yourLove = 0.2;
+var sum = love + yourLove;
+console.log(sum);
+//.............................
+var love = 0.1;
+var yourLove = 0.2;
+var sum = love + yourLove;
+sum = sum.toFixed(1)
+sum = parseFloat(sum)
+console.log( sum);
