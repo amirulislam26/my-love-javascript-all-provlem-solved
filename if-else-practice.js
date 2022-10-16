c4a47f2a59a3025e77f46047969a689e558f3186
amirulislam26 <26techamirulislam@gmail.com>
2022-10-16 07:12:29 -0700

    if else practice added my all js

---
+++ if-else-practice.js
@@ -0,0 +1,85 @@
+var myLove = 100;
+var yourLove = 90;
+var car = 1;
+//...................A.......................
+// if(yourLove < myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//...................B.......................
+// if(yourLove > myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//....................C......................
+// if(yourLove => myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//.....................D.....................
+// if(yourLove >= myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//....................E......................
+// if(yourLove == myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//...................F.......................
+// if(yourLove === myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//.....................G.....................
+// if(yourLove && myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+//..................H........................
+// if(yourLove || myLove){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+//..................I........................
+// if(yourLove && myLove > 150){
+//     console.log('ami tumoy onk valobashi')
+// }
+// else(
+//     console.log('tore ami valobashi na')
+// )
+
+
+//..................J........................
+if(yourLove && myLove > 150 || car){
+    console.log('ami tumoy onk valobashi')
+}
+else(
+    console.log('tore ami valobashi na')
+)
+
+
+//..................J........................
